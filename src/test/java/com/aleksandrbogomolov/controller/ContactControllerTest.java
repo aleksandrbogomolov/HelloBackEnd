86c76e7a9ff4191d396085543c55513733e0ea5c
Aleksandr Bogomolov <ksandr.ua@gmail.com>
2016-09-24 12:39:28 +0300

    Chane server.port for test

--- src/test/java/com/aleksandrbogomolov/controller/ContactControllerTest.java
+++ src/test/java/com/aleksandrbogomolov/controller/ContactControllerTest.java
@@ -13,12 +13,19 @@ import static org.junit.Assert.*;
  * Created by aleksandrbogomolov on 9/24/16.
  */
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT, properties = "server.port=8888")
 public class ContactControllerTest {
 
     @Test
     public void getFilteredContacts() throws Exception {
-        when().get("http://localhost:8080/hello/contacts?nameFilter=^A.*$&offset=0&limit=5")
+        when().get("http://localhost:8888/hello/contacts?nameFilter=^A.*$&offset=0&limit=5")
+              .then().statusCode(200).contentType("application/json;charset=UTF-8")
+              .body("name", Matchers.hasItems("Boris", "Fedor", "Grigoriy", "Petrov"));
+    }
+
+    @Test
+    public void getFilteredContactsWithoutParameters() throws Exception {
+        when().get("http://localhost:8888/hello/contacts?nameFilter=^A.*$")
               .then().statusCode(200).contentType("application/json;charset=UTF-8")
               .body("name", Matchers.hasItems("Boris", "Fedor", "Grigoriy", "Petrov"));
     }
